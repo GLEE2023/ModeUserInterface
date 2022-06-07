b3a996a32d0c7bdaa15977cea1195defe8bb3c14
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-06-07 13:43:38 -0600

    Temperature sensor form

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,16 @@ def about():
 	sites = ['Temperature', 'Accelerometer', 'Thermopile', 'Magnetometer', 'Capacitor']
 	return render_template("about.html", sites=sites)
 
-@app.route("/tmp117")
+@app.route("/tmp117", methods=['GET','POST'])
 def tmp117():
-	return render_template("TMP117.html")
+    if(request.method == 'GET'):
+        return render_template("TMP117.html")
+    elif(request.method == 'POST'):
+        convCycle = eval(request.form['convCycle'].split(', ')[0])#BAD PRACTICE! DONT USE!
+        #convCycle is a tuple, (conversion cycle time, averaging)
+        activeConversionTime = convCycle[1]*0.0155
+        standbyTime = convCycle[0] - activeConversionTime
+        amps = ((135*activeConversionTime) + (1.25*standbyTime))/convCycle[0]
+        debugArray = [convCycle, activeConversionTime, standbyTime, amps*3.3]
+
+        return render_template("TMP117.html", debugArray=debugArray)
--- scripts/math.js
+++ scripts/math.js
@@ -1,1 +1,15 @@
 //calculation of power based on modes
+function getPower(){
+  let modeValue = document.getElementsByName("modes").value;
+  let averagingTable = document.getElementsByName("convTable");
+  for (var i = 0, row; row = averagingTable.rows[i]; i++) {
+   //iterate through rows
+   //rows would be accessed using the "row" variable assigned in the for loop
+    for (var j = 0, col; col = row.cells[j]; j++) {
+       //iterate through columns
+       //columns would be accessed using the "col" variable assigned in the for loop
+       console.log(table.rows[r].cells[c].innerHTML);
+    }
+  }
+  return false
+}
