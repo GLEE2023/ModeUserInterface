71f55df48d654d5a09e13835bc1755ea85076e79
caxi9064 <70486350+caxi9064@users.noreply.github.com>
2022-06-08 11:05:40 -0600

    everything

--- home.py
+++ home.py
@@ -14,28 +14,25 @@ def about():
 @app.route("/accelerometer")
 def accelerometer():
 	return render_template("accelerometer.html")
- 
+
 @app.route("/magnetometer")
 def magnetometer():
 	return render_template("magnetometer.html")
-
-@app.route("/TMP117")
-def TMP117():
-	return render_template("TMP117.html")
 	
 @app.route("/home")
 def home():
 	return render_template("home.html")
- 
+
 app.run(host='localhost', port=5000)
-@app.route("/tmp117", methods=['GET','POST'])
-def tmp117():
+@app.route("/everything", methods=['GET','POST'])
+def everything():
     if(request.method == 'GET'):
-        return render_template("TMP117.html")
+        return render_template("everything.html")
     elif(request.method == 'POST'):
         convCycle = request.form['convCycle'].split(',')
         activeConversionTime = float(convCycle[1])*0.0155
         standbyTime = float(convCycle[0]) - activeConversionTime
         amps = ((135*activeConversionTime) + (1.25*standbyTime))/float(convCycle[0])
 
-        return render_template("TMP117.html", power=amps*3.3)
+
+        return render_template("everything.html", power=amps*3.3)
