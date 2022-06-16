2bbc528d9997251a334dde8d695dc9217c8cb20a
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-06-16 11:06:48 -0600

    accel changes

--- sensors/accelerometer.py
+++ sensors/accelerometer.py
@@ -2,50 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.widgets import Slider, RadioButtons
+from Sensor import Sensor
 
 
-class MPU6050():
+class MPU6050(Sensor):
 
+    #DEPRECATED! Thought I would keep around just in case.
+    # def __init__ (self, time_step, duration, sample_rate_divisor=0, mode="accelerometer_only", low_power_wakeup=0, digital_low_pass=0, loop_rate=60):
+    #     #mode tells the type of mode the sensor is in. Choices for accelerometer are "accelerometer_only", "gyroscope_only",
+    #     #"gyroscope_DMP", "gyroscope_accelerometer", "gyroscope_accelerometer_DMP"
 
-    def __init__ (self, time_step, duration, sample_rate_divisor=0, mode="accelerometer_only", low_power_wakeup=0, digital_low_pass=0, loop_rate=60):
-        #mode tells the type of mode the sensor is in. Choices for accelerometer are "accelerometer_only", "gyroscope_only",
-        #"gyroscope_DMP", "gyroscope_accelerometer", "gyroscope_accelerometer_DMP"
+    #     #time_step would define at what intervals (and therefore time) the model
+    #     # would return data at. A time_step o 1 second would have the model return
+    #     # data at 0,1,2,3... seconds assuming start time of 0 seconds.
+    #     #
+    #     self.mode = mode
+    #     self.time_step = time_step
+    #     self.duration = duration
 
-        #time_step would define at what intervals (and therefore time) the model
-        # would return data at. A time_step o 1 second would have the model return
-        # data at 0,1,2,3... seconds assuming start time of 0 seconds.
-        #
-        self.mode = mode
-        self.time_step = time_step
-        self.duration = duration
-
-        self.low_power_wakeup = low_power_wakeup
-        #In Hz, determines how fast the sensor wakes up when in low power mode. More wakeups means more power used.
-        #Choices are 1.25, 5, 20, 40.
-        self.sample_rate_divisor = sample_rate_divisor
-        #Value between 0 and 255. Used as a divisor for sample rate equation, higher means slower sample rate meaning
-        #presumably lower data collected per second.
-        self.digital_low_pass = digital_low_pass
-        #Digital low pass filter. Enabling this lowers the sampling rate by a factor of 8. See page 12 of the 
-        #register map linked below. 
-        self.loop_rate = loop_rate
-        #how fast the arduino loop will run. This value is kind of up in the air, but we predict that it may have an
-        #affect on how fast we can read from the sensors depending on how much code is run in the loop, in Hz.
-        self.time = np.arange(0,self.duration,self.time_step)
+    #     self.low_power_wakeup = low_power_wakeup
+    #     #In Hz, determines how fast the sensor wakes up when in low power mode. More wakeups means more power used.
+    #     #Choices are 1.25, 5, 20, 40.
+    #     self.sample_rate_divisor = sample_rate_divisor
+    #     #Value between 0 and 255. Used as a divisor for sample rate equation, higher means slower sample rate meaning
+    #     #presumably lower data collected per second.
+    #     self.digital_low_pass = digital_low_pass
+    #     #Digital low pass filter. Enabling this lowers the sampling rate by a factor of 8. See page 12 of the 
+    #     #register map linked below. 
+    #     self.loop_rate = loop_rate
+    #     #how fast the arduino loop will run. This value is kind of up in the air, but we predict that it may have an
+    #     #affect on how fast we can read from the sensors depending on how much code is run in the loop, in Hz.
+    #     self.time = np.arange(0,self.duration,self.time_step)
+    
+    def __init__(self, **params):
+        super(MPU6050, self).__init__(**params)
+        #All params configured in parent function!
 
     def runSim(self, mode):
         #active_times would be a list of tuples/list that would define the time at which the sensor was running
         # i.e. [[0,1],[4,5],[6,7]] would have the sensor running from time 0s to 1s,
         # 4s to 5s, 6s to 7s
-        #time = np.arange(0,self.duration,self.time_step) #time at which to collect data
+        time = np.arange(0,self.duration,self.time_step) #time at which to collect data
         #active_vec: we are assuming that the accelerometer will constantly be on, since the slowest it will update is
         #once every 0.8 seconds.
-        power = self.getPowerUsage(mode)
+        power = self.getPowerUsage(mode, time)
         data = self.getDataAccumulated()
 
-        return self.time, power, data
+        return time, power, data
 
-    def getPowerUsage(self, mode):
+    def getPowerUsage(self, mode, time):
         #Calculates power when sensor is active. This value is used in conjunction with 
         self.mode = mode
         power_used = 0
@@ -78,8 +83,7 @@ class MPU6050():
             print("Invalid mode entered.")
             return -1
         
-        power_usage = np.where(self.time, power_used, self.time)
-        return power_usage
+        return Sensor.getPowerUsage(self, power_used, time)
         #returns a vector of when power is used. Units are in mW.
 
     def getDataAccumulated(self):
@@ -103,42 +107,6 @@ class MPU6050():
         data_accumulated = np.linspace(0, bytes_over_duration, num=int(self.duration / self.time_step))
         return data_accumulated
 
-    def getActiveTime(self, time):
-        #this function should only matter when used in conjunction with low_power_wakeup. Otherwise the accelerometer
-        #runs at 1kHz and can be assumed to be always on.
-        period = 1 / self.low_power_wakeup
-        standby_time = 1 - period
-        active_times = []
-        print(active_times)
-        time = time.tolist()
-        length = len(time)
-        standby = 5 / 1000 # default power when off
-        arr = [standby] * len(time) # creating corresponding power array to time intervals, default values 
-
-        # check if the given start and end time is a valid value in the time array and round to nearest value 
-        for times in active_times:
-            start = times[0]
-            end = times[1]
-            if times[0] not in time:
-                print("start or end time is invalid, rounding to nearest whole number")
-                start = round(start)
-
-            if times[1] not in time:
-                print("start or end time is invalid, rounding to nearest whole number")
-                end = round(end)
-
-            start_index = time.index(start) 
-            end_index = time.index(end) 
-
-            # calculate power here for active conversion period
-            # if statement for power vs data
-            
-            power = self.calculate_power(time)
-            for i in range(start_index, end_index+1):
-                arr[i] = power   
-            
-        return arr
-
     def setSampleRate(self, samplerate):
         self.sample_rate_divisor = samplerate
 
@@ -149,7 +117,7 @@ class MPU6050():
             return 12
 
 
-sensor = MPU6050(time_step=0.1, duration=10, mode="accelerometer_only", low_power_wakeup=0, loop_rate=1000)
+sensor = MPU6050(time_step=0.1, duration=10, mode="accelerometer_only", low_power_wakeup=0, loop_rate=1000, digital_low_pass=0, sample_rate_divisor=0)
 dude = sensor.runSim("accelerometer_only")
 
 f = plt.figure(figsize=(10,10))
